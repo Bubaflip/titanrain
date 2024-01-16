12044a32690b3410008eca5a4ee592fa6113080d
Saturn <martinboisrond@gmail.com>
2024-01-16 18:49:11 +0000

    new font

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "adab95af66a55bee"; }
+/******/ 		__webpack_require__.h = function() { return "b69639eed5743213"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
